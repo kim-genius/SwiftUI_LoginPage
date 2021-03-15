efd9f12a3e7fb13ba072f41e2846c3e1120378d8
kim-genius <thdudeoddl@naver.com>
2021-03-15 12:50:37 +0900

    Initial Commit

---
+++ LoginPageEN/LoginPageENApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPageENApp.swift
+//  LoginPageEN
+//
+//  Created by kimyoungjae on 2021/03/15.
+//
+
+import SwiftUI
+
+@main
+struct LoginPageENApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
